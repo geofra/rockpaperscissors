387deb8a8bf029c5e848f51fa60952a83fdbf840
geofra <192031279+geofra@users.noreply.github.com>
2025-03-29 18:45:25 +0200

    Reorganize game, finalize logic and create acceptable outcome

--- jscript.js
+++ jscript.js
@@ -1,65 +1,70 @@
-
 function getComputerChoice() {
-    let compstring;
-    let decidervalue = Math.floor(Math.random() * 4);
-    if (decidervalue == 1) {
-        compstring = "rock";
+    let companswer;
+    let rinteger = Math.floor(Math.random() * 3);
+    if (rinteger == 1) {
+        companswer = "rock";
     }
-    else if (decidervalue == 2) {
-        compstring = "paper";
+    else if (rinteger == 2) {
+        companswer = "paper";
     }
-    else {
-        compstring = "scissors";
+    else if (rinteger == 3) {
+        companswer = "scissors"
     }
-    return (compstring); 
+    return companswer;
 }
 
 function getHumanChoice() {
-    let humstring = prompt("Valid string input required: rock, paper or scissors").toLowerCase();
-    return (humstring);
+    let humchoice = prompt("What's your choice human!?").toLowerCase();
+    return humchoice;
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+let humScore = 0;
+let compScore = 0;
+function playRound(humanChoice, computerChoice) {
+     humanChoice = getHumanChoice();
+     computerChoice = getComputerChoice();
+    if (humanChoice == computerChoice) {
+        console.log("DRAW! You both chose the same material!");
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("WIN! Rock decimates scissors!");
+        humScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("LOSE! Scissors cut through rock!");
+        compScore++; 
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("LOSE! Rock decimates scissors");
+        compScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("WIN! Paper wraps around rock and strangles it!");
+        humScore++;
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("LOSE! Your rock is left gasping for air by their paper!");
+        compScore++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("WIN! Scissors cut through paper like a cake!");
+        humScore++;
+    }
+    return console.log(humScore);
+}
 
-let humanScore = 0;
-let computerScore = 0;
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
+if (humScore > compScore) {
+    return console.log(`WOOHOO! YOU WON ${humScore} vs ${compScore}`)
+}
+else {
+    return console.log(`I'm sorry to be the one to break this to you...${humScore} vs ${compScore}`)
+}
 
-function playRound (humanSelection, computerSelection) {
-    if (humanSelection == computerSelection) {
-        console.log(`You both chose ${computerSelection} and have thus tied!`)
-    }
-    else if (humanSelection == "rock" && computerSelection == "paper") {
-        console.log("You win! Rock beats Paper!")
-        humanScore++;
-    }
-    else if (humanSelection == "paper" && computerSelection == "rock") {
-        console.log("You lose! Your paper is beaten by Rock!")
-        computerScore ++;
-    }
-    else if (humanSelection == "scissors" && computerSelection == "rock") {
-        console.log("You lose! Your scissors are beaten by Rock!")
-        computerScore++;
-    }
-    else if (humanSelection == "scissors" && computerSelection == "paper") {
-        console.log("You win! Your scissors cut through Rock!")
-        humanScore++;
-    }
-    else if (humanSelection == "paper" && computerSelection == "scissors") {
-        console.log("You lose! Your paper is beaten by scissors!")
-        computerScore++;
-    }
-    else if (humanSelection == "rock" && computerSelection == "scissors") {
-        console.log("You win! Your Rock decimates scissors!")
-        humanScore++;
-    }
-    else {
-        console.log("Something went wrong, try agaim")
-    }
-    }
-
-
-
-
-
-
+}
+playGame();
